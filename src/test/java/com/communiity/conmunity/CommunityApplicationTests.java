2d1120e0c3fb34eeac6cf5de177a8d294538ae51
steve_combo <2445540272@qq.com>
2019-11-30 13:39:47 +0800

    init repo

---
+++ src/test/java/com/communiity/conmunity/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communiity.conmunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
