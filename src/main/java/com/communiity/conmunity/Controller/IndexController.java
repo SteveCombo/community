5674ed8b9b6e5ffa31f67bc0927bcb7bf2bfb094
steve_combo <2445540272@qq.com>
2019-12-07 23:30:48 +0800

    continue debugs , i am tired

--- src/main/java/com/communiity/conmunity/Controller/IndexController.java
+++ src/main/java/com/communiity/conmunity/Controller/IndexController.java
@@ -24,7 +24,6 @@ public class IndexController {
                 break;
             }
         }
-
         return "index";
     }
 }
--- src/main/java/com/communiity/conmunity/mapper/QuestionMapper.java
+++ src/main/java/com/communiity/conmunity/mapper/QuestionMapper.java
@@ -4,8 +4,12 @@ import com.communiity.conmunity.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface QuestionMapper {
     @Insert("insert into QUESTION(title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
+
+    List<Question> list();
 }
